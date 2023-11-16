18bc96871243aa7c44786d5b1b88e82b222e36b9
FrancescoMazzocchi <123@email.it>
2023-11-16 15:11:49 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const { createApp } = Vue;
+
+
+
+const option = {
+    data: function () {
+        return {
+            slides: [
+                {
+                    immagine: 'https://img.freepik.com/free-vector/welcome-pattern-different-languages_23-2147870141.jpg',
+                    titolo: 'Benvenuti nel nostro sito',
+                    testo: 'Esplora la nostra vasta gamma di prodotti e scopri offerte speciali.'
+                },
+                {
+                    immagine: 'https://img.freepik.com/free-vector/spring-sale-instagram-post_23-2148849921.jpg',
+                    titolo: 'Nuova collezione primavera',
+                    testo: 'Scopri le ultime tendenze della moda con la nostra collezione primavera.'
+                },
+                {
+                    immagine: 'https://img.freepik.com/free-vector/special-offer-modern-sale-banner-template_1017-20667.jpg',
+                    titolo: 'Offerte speciali',
+                    testo: 'Approfitta delle nostre offerte speciali. Sconti incredibili solo per te!'
+                }
+            ],
+        }
+    },
+
+    methods: {
+
+    }
+
+
+
+
+};
+
+
+createApp(option).mount('#app')
